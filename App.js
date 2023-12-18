f4ff61c17c1887f645e036bd93ab1ef4e0c4568b
g-popp <Georg.Popp@outlook.com>
2023-12-18 13:52:38 +0100

    add: Layout changes and lint fix

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>MealsToGo!</Text>
-            <StatusBar style='auto' />
-        </View>
+        <>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.search}>
+                    <Text>search</Text>
+                </View>
+                <View style={styles.list}>
+                    <Text>list</Text>
+                </View>
+            </SafeAreaView>
+            <ExpoStatusBar style="auto" />
+        </>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        fontSize: 40,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+        marginTop: StatusBar.currentHeight,
+    },
+    search: {
+        padding: 16,
+        backgroundColor: "green",
+    },
+    list: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: "blue",
+    },
 });
