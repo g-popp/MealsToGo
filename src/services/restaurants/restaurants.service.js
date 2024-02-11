f9bd1378e9521f4d7ac5e457fcbec5d9bdb5a992
g-popp <Georg.Popp@outlook.com>
2024-02-11 14:42:31 +0100

    Fix restaurantsTransform export

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -14,7 +14,8 @@ export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
         resolve(mock);
     });
 };
-const restaurantsTransform = ({ results = [] }) => {
+
+export const restaurantsTransform = ({ results = [] }) => {
     const mappedResults = results.map((restaurant) => {
         return {
             ...restaurant,
